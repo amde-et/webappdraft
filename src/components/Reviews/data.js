0efd9bb330b471cfc483f075a3f021ce7b5ca6e9
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2022-12-22 19:36:04 +0300

    Update data.js

--- src/components/Reviews/data.js
+++ src/components/Reviews/data.js
@@ -1,9 +1,9 @@
 const data = [
   {
-    name: 'Amdebrihan Abebe',
-    job: 'Founder CEO and Software Engineer',
+    name: 'Faskawe Berhanu and Chrstian Abreham ',
+    job: 'Founders CEO and Software Engineer',
     image: './images/amde.jpg',
-    desc: `Amdebirhan Abebe a Passionate Full Stack Developer specialized in Back end Development . Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise, high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.`,
+    desc: `Chrstian Abreham  and Faskawe Berhanu are  Passionate Full Stack Developer specialized in Back end Development . We are freinds adn raising software enginners. Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise, high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.`,
   },
   
 ];
