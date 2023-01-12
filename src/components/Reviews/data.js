cd2f898eb047144fbf164522b0007c06a5bdd54d
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-12 04:46:15 -1200

    Update data.js

--- src/components/Reviews/data.js
+++ src/components/Reviews/data.js
@@ -4,7 +4,8 @@ const data = [
     job: 'CEO, Founder and Software Engineer',
     image: './images/amde.jpg',
     desc: `Amdebirhan Workeye Abebe, A Passionate  and raisingSoftware engineers and Young entrepreneur.  Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise,low and  high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.
-                            Let’s shape the future of  Computer!.`,
+                                                                                                                                                                                                                                                       
+            Let’s shape the future of  Computers !`,
   },
   
 ];
