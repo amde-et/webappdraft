42cd2ff2947b1ee3e4d32418043e16cc341ea217
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-12 04:56:22 -1200

    Update data.js

--- src/components/Reviews/data.js
+++ src/components/Reviews/data.js
@@ -3,9 +3,10 @@ const data = [
     name: 'Amdebirhan Abebe ',
     job: 'CEO, Founder and Software Engineer',
     image: './images/amde.jpg',
-    desc: `Amdebirhan Workeye Abebe, A Passionate  and raisingSoftware engineers and Young entrepreneur.  Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise,low and  high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.
-                                                                                                                                                                                                                                                       
-            Let’s shape the future of  Computers !`,
+    desc: `Amdebirhan Workeye Abebe is a rising software engineer and young entrepreneur. A budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise low- and high-level programming Currently, I am attempting to establish a solid technical and organizational foundation in software engineering. I am a leader and team member accustomed to making meaningful bonds with community members to share experiences and bring about change.
+
+             Let's  shape computers' futures today!  `,
+           
   },
   
 ];
