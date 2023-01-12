9e33eef8a816f52a660dc1b5ed2b120fb11b59d8
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-12 04:31:47 -1200

    Update data.js

--- src/components/Reviews/data.js
+++ src/components/Reviews/data.js
@@ -1,9 +1,10 @@
 const data = [
   {
-    name: 'Faskawe Berhanu and Chrstian Abreham ',
-    job: 'Founders CEO and Software Engineer',
+    name: 'Amdebirhan Abebe ',
+    job: 'CEO, Founder and Software Engineer',
     image: './images/amde.jpg',
-    desc: `Chrstian Abreham  and Fasikaw Birhan are  Passionate Software engineers and Full Stack Developer specialized in Back end Development and Front end Development respectively . We are freinds adn raising software enginners.We want to pursue our higher education in software engineering at one of the most pretgious university in ethiopia , Bahir Dar University. We want to make this startup company to be big as google and Amazon and we will if we can get a good software engineering courses at Bahir dar Universit. Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise, high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.`,
+    desc: `Amdebirhan Workeye Abebe, A Passionate  and raisingSoftware engineers and Young entrepreneur.  Budding computer scientist dedicated to solving the diverse issues that plague our communities through innovative software systems built with precise,low and  high-level programming. Currently looking to establish a firm technical and organizational background in software engineering s. Dependable team member accustomed to making meaningful bonds with community members to share experiences and bring about change.
+                            Let’s shape the future of  Computer!.`,
   },
   
 ];
