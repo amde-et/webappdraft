dce99d5a5ff9975bf967f9d300fafe232ca787c9
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-13 23:04:28 -1200

    Update data.js

--- src/components/About/data.js
+++ src/components/About/data.js
@@ -1,5 +1,5 @@
 const data =[
-    {title:'Services',desc:'We offer you a broad variety of Software Development and IT related services. Our core focus is Outstaffing. We recruit and set up dedicated teams that you manage just as you would full-time employees. We also provide regular Outsourcing services and can assist with Legal Entity Setup for clients who wish to establish a presence in Ukraine ',image:'image'},
+    {title:'Services',desc:'We offer you a broad variety of software development and IT-related services. Our services range from customized software development, Full Stack Development,mobile app development, system administration, and any other tech-related Projects. My company works with the best software engineers in the nation, and we are aiming to be the leading tech company by 2024 in East Africa. ',image:'image'},
     {
         title:'Who we are',desc:'My company is the Software Development Company located in Ethiopia. Specialize in custom software development on JavaScript, AngularJS, Python, .NET, Web, Design, iOS, Android, QA. We work with our clients to provide technology professionals to help them achieve their goals. We listen to our clients, we understand their needs and do whatever it takes to build a highly productive technical team at a competitive price.',image:'image'
     },
